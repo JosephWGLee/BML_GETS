d20dddda2cf08d7cd3d4b9310e6c782686a7f646
Joseph Lee <148720389+JosephWGLee@users.noreply.github.com>
2026-03-19 21:06:50 +0000

    Add files via upload

---
+++ scripts/gpcounts.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import os
+from IPython.display import display
+import scipy.stats as stats
+from statsmodels.stats.multitest import multipletests
+import tensorflow as tf
+import gpflow
+from GPcounts.RNA_seq_GP import rna_seq_gp
+
+counts_df = pd.read_csv(r"all_normalised_counts.csv")
+sampletable_df = pd.read_csv(r"all_sampletable.csv")
+
+#CSV processing 
+
+counts_df = counts_df.rename(columns={"Unnamed: 0": "ID"})
+counts_df.set_index(counts_df.columns[0], inplace=True)
+
+counts_df = counts_df.round().astype(int)
+counts_df = counts_df.loc[(df.iloc[:, 1:] > 0).sum(axis=1) > 15]
+counts_df.set_index(counts_df.columns[0], inplace = True)
+
+X = sampletable_df[["Time"]]
+Y = counts_df.index.tolist()
+
+#gpcounts
+
+sparse = True
+gp_counts = rna_seq_gp(X,Y.loc[gene_names],M=10, sparse=sparse) 
+lik_name = "Negative_binomial" 
+results = gp_counts.Two_samples_test(lik_name)
+results_df = pd.DataFrame(results)
+
+#Statistical tests 
+
+llr_values = results.loc[:,"log_likelihood_ratio"]
+
+degrees_freedom = 1
+p_values = [1 - stats.chi2.cdf(abs(llr), degrees_freedom) for llr in llr_values]
+
+results_df_chisq = 1
+results_df["p_value"] = 1 - stats.chi2.cdf(abs(results_df["log_likelihood_ratio"]), results_df_chisq)
+
+p_value = results_df["p_value"].values
+rejected, q_values, _, _ = multipletests(p_values, method='fdr_bh')
+results_df["q_value"] = q_values
+results_df["significant"] = rejected
+
+results_df.to_csv("all_gpcounts_significant_genes.csv", index=True)
+
+#Kernel fitting
+
+counts_array = Y.values
+counts_array = np.array(Y.values, dtype=np.float64)
+
+time_points_array = np.array(X.values, dtype=np.float64)
+time_points_array = time_points_array.reshape(-1,1)
+
+rbf_kernel = gpflow.kernels.SquaredExponential()
+periodic_kernel = gpflow.kernels.Periodic(base_kernel=rbf_kernel)
+combined_kernel = rbf_kernel + periodic_kernel
+
+file_path = "Only_RBF_Periodicity_Values.csv"
+if os.path.exists(file_path):
+    os.remove(file_path)
+
+results = []
+
+for gene_index in range(counts_array.shape[0]):
+    
+    y = counts_array[gene_index, :].reshape(-1, 1)
+
+    y = tf.convert_to_tensor(y, dtype=tf.float64)
+
+    rbf_value = rbf_kernel(time_points_array, time_points_array)
+    periodic_value = periodic_kernel(time_points_array, time_points_array)
+    combined_value = rbf_value + periodic_value
+
+    rbf_kernel= gpflow.kernels.SquaredExponential()
+    periodic_kernel = gpflow.kernels.Periodic(base_kernel=gpflow.kernels.SquaredExponential())
+    combined_kernel = rbf_kernel + periodic_kernel 
+
+    model = gpflow.models.GPR(data=(time_points_array, y), kernel=combined_kernel)
+    gpflow.optimizers.Scipy().minimize(model.training_loss, model.trainable_variables)
+    
+    ratio = rbf_value/combined_value
+
+    results.append({
+        "Gene Name" : gene_index,
+        "RBF Value": np.mean(rbf_value.numpy()),
+        "Peridodicity Value": np.mean(periodic_value.numpy()),
+        "Combined Value": np.mean(combined_value.numpy()),
+        "Ratio RBF:Periodicity": np.mean(ratio.numpy())
+    })
+
+kernel_df = pd.DataFrame(results)
+kernel_df.to_csv("all_rbf_periodicity_values.csv", index=False, mode="w")
+existing_df = pd.read_csv("all_gpcounts_significant_genes.csv")
+
+updated_df = pd.concat([existing_df, kernel_df.iloc[:, 1:]], axis=1)
+updated_df = updated_df.round(3)
+updated_df.to_csv("final_gpcounts.csv", index=False)
